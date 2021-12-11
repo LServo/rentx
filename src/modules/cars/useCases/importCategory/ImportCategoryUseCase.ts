b20ffb6ded73527a7ddc29ce3e425e0ad2377312
Lucas Servo <l.servo@hotmail.com>
2021-12-11 07:41:21 -0300

    feat => added documentation with swagger

--- src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
+++ src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
@@ -1,15 +1,53 @@
 import { parse as csvParse } from "csv-parse";
 import fs from "fs";
 
+import { ICategoriesRepository } from "../../repositories/ICategoriesRepository";
+
+interface IImportCategory {
+    name: string;
+    description: string;
+}
+
 class ImportCategoryUseCase {
-    execute(file: Express.Multer.File): void {
-        const stream = fs.createReadStream(file.path);
-        const parseFile = csvParse(); // por padrão o método já reconhece que o delimitador á a vírgula, então não precisamos passar nada
+    constructor(private categoriesRepository: ICategoriesRepository) {}
 
-        stream.pipe(parseFile);
+    loadCategories(file: Express.Multer.File): Promise<IImportCategory[]> {
+        return new Promise((resolve, reject) => {
+            const stream = fs.createReadStream(file.path);
+            const categories: IImportCategory[] = [];
+            const parseFile = csvParse(); // por padrão o método já reconhece que o delimitador á a vírgula, então não precisamos passar nada
 
-        parseFile.on("data", async (line) => {
-            console.log(line);
+            stream.pipe(parseFile);
+
+            parseFile
+                .on("data", async (line) => {
+                    const [name, description] = line;
+                    categories.push({
+                        name,
+                        description,
+                    });
+                })
+                .on("end", () => {
+                    fs.promises.unlink(file.path); // apagando o arquivo temporário
+                    resolve(categories);
+                })
+                .on("error", (err) => reject(err));
+        });
+    }
+    async execute(file: Express.Multer.File): Promise<void> {
+        const categories = await this.loadCategories(file);
+
+        categories.map(async (category) => {
+            const { name, description } = category;
+
+            const existCategory = this.categoriesRepository.findByName(name);
+
+            if (!existCategory) {
+                this.categoriesRepository.create({
+                    name,
+                    description,
+                });
+            }
         });
     }
 }
