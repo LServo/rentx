0617e7b45f8860c0b32a274de40454796fcdfc64
Lucas Servo <l.servo@hotmail.com>
2021-12-11 04:28:02 -0300

    feat => added a singleton pattern on repository instances

--- src/modules/cars/repositories/implementations/CategoriesRepository.ts
+++ src/modules/cars/repositories/implementations/CategoriesRepository.ts
@@ -1,14 +1,26 @@
-import { Category } from "../models/Category";
+import { Category } from "../../models/Category";
 import {
     ICategoriesRepository,
     ICreateCategoryDTO,
-} from "./ICategoriesRepository";
+} from "../ICategoriesRepository";
 
 class CategoriesRepository implements ICategoriesRepository {
     private categories: Category[];
 
-    constructor() {
+    private static INSTANCE: CategoriesRepository; // mesmo tipo da nossa classe
+
+    private constructor() {
+        // a partir de agora não queremos que o new CategoriesRepository() seja possível, ou seja, somente a noss classe vai poder instanciar
         this.categories = [];
+    }
+
+    public static getInstance(): CategoriesRepository {
+        // esse método vai ser responsável por criar uma instancia, ou repassar uma já existente para quem requisitar
+
+        if (!CategoriesRepository.INSTANCE) {
+            CategoriesRepository.INSTANCE = new CategoriesRepository();
+        }
+        return CategoriesRepository.INSTANCE;
     }
 
     create({ name, description }: ICreateCategoryDTO): void {
