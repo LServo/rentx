64179cc421cb7dea992b0ba77b452d21566c563d
Lucas Servo <l.servo@hotmail.com>
2021-12-21 08:09:00 -0300

    feat => added the supertest server, to run interation tests directly in the application

--- src/modules/rentals/useCases/createRental/CreateRentalController.spec.ts
+++ src/modules/rentals/useCases/createRental/CreateRentalController.spec.ts
@@ -1,1 +1,9 @@
 import request from "supertest";
+
+import { app } from "@shared/infra/http/app";
+
+describe("Create Category Controller", async () => {
+    it("test", async () => {
+        await request(app).get("/cars/available").expect(201);
+    });
+});
