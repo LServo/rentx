f80c6bd8809295f41bb9e76ea0e90c965d699ba7
Lucas Servo <l.servo@hotmail.com>
2021-12-27 17:35:35 -0300

    feat => added reset password feature

--- src/shared/infra/http/routes/password.routes.ts
+++ src/shared/infra/http/routes/password.routes.ts
@@ -1,16 +1,19 @@
 import { Router } from "express";
 
+import { ResetPasswordUserController } from "@modules/accounts/useCases/resetPasswordUser/ResetPasswordUserController";
 import { SendForgotPasswordMailController } from "@modules/accounts/useCases/sendForgotPasswordMail/SendForgotPasswordMailController";
 
 import { ensureAuthenticated } from "../middlewares/ensureAuthenticated";
 
 const passwordRoutes = Router();
 const sendForgotPasswordMailController = new SendForgotPasswordMailController();
+const resetPasswordUserController = new ResetPasswordUserController();
 
 passwordRoutes.post(
     "/forgot",
     ensureAuthenticated,
     sendForgotPasswordMailController.handle
 );
+passwordRoutes.post("/reset", resetPasswordUserController.handle);
 
 export { passwordRoutes };
