9fc7d67143f6994dbbf8bda40d9849efdd1f14a7
Lucas Servo <l.servo@hotmail.com>
2021-12-29 16:54:11 -0300

    fix => "UpdateUserAvatarUseCase", "car.routes" and "users.routes" are refactored

--- src/shared/container/providers/StorageProvider/implementations/LocalStorageProvider.ts
+++ src/shared/container/providers/StorageProvider/implementations/LocalStorageProvider.ts
@@ -1,11 +1,29 @@
+import fs from "fs";
+import { resolve } from "path";
+
+import upload from "@config/upload";
+
 import { IStorageProvider } from "../IStorageProvider";
 
 class LocalStorageProvider implements IStorageProvider {
-    save(file: string): Promise<string> {
-        throw new Error("Method not implemented.");
+    async save(file: string, folder: string): Promise<string> {
+        await fs.promises.rename(
+            resolve(upload.tmpFolder, file), // tirando o arquivo desta pasta
+            resolve(`${upload.tmpFolder}/${folder}`, file) // passando para essa
+        );
+
+        return file;
     }
-    delete(file: string): Promise<void> {
-        throw new Error("Method not implemented.");
+    async delete(file: string, folder: string): Promise<void> {
+        const filename = resolve(`${upload.tmpFolder}/${folder}`, file); // passando para essa
+
+        try {
+            await fs.promises.stat(filename); // tenta retornar informações sobre o arquivo
+        } catch {
+            return; // se não conseguir é porque já foi deletado, então, apenas retorna
+        }
+
+        await fs.promises.unlink(filename); // se conseguir é porque o arquivo existe, então, deleta
     }
 }
 
--- src/shared/container/providers/index.ts
+++ src/shared/container/providers/index.ts
@@ -4,6 +4,8 @@ import { IDateProvider } from "./DateProvider/IDateProvider";
 import { DayjsDateProvider } from "./DateProvider/implementations/DayjsDateProvider";
 import { IMailProvider } from "./MailProvider/IMailProvider";
 import { EtherealMailProvider } from "./MailProvider/implementations/EtherealMailProvider";
+import { LocalStorageProvider } from "./StorageProvider/implementations/LocalStorageProvider";
+import { IStorageProvider } from "./StorageProvider/IStorageProvider";
 
 container.registerSingleton<IDateProvider>(
     "DayjsDateProvider",
@@ -14,6 +16,10 @@ container.registerInstance<IMailProvider>(
     "EtherealMailProvider",
     new EtherealMailProvider()
 );
-
 // precisa ser injetado assim que a aplicação é iniciado, para que seja possível criar o client antes que o sendMail seja chamado
 // mesmo utilizando o registerInstance, o tsyringe vai utilizar o conceito de singleton, então o EtherealMailProvider só vai ser instanciado uma vez
+
+container.registerSingleton<IStorageProvider>(
+    "StorageProvider",
+    LocalStorageProvider
+);
