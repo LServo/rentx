c7d9d9c850d0f86c9b5bed7c41b224c849541a7e
Lucas Servo <l.servo@hotmail.com>
2022-01-15 11:29:59 -0300

    fix => fixed cached files of git history

--- dist/shared/container/providers/DateProvider/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-
-var _tsyringe = require("tsyringe");
-
-var _DayjsDateProvider = require("./implementations/DayjsDateProvider");
-
-_tsyringe.container.registerSingleton("DayjsDateProvider", (0, _tsyringe.delay)(() => _DayjsDateProvider.DayjsDateProvider));
--- dist/shared/infra/http/server.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-
-var _app = require("./app");
-
-_app.app.listen(3000, () => console.log("Server is running on port 3000"));
